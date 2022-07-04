9f79e29f7a2cca9983d3a4e74587d19ceb27bb8b
davidalejandrolazopampa <david.lazo@utec.edu.pe>
2022-07-03 23:17:36 -0500

    Kd_tree.py finish

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Tesis_p2_KNN_Classifier
+
+# Integrant
+|  **#** | **Código** | **Apellidos, Nombre** | **% Trabajo** |
+| :---: | :---: | :---: | :---: |
+|  1 | 201810010 | Lazo Pampa, David Alejandro| 100% |
+|  2 | 201810000 | Barreto Zavaleta, Jeanlee | 100% |
+|  3 | 201810000 | Barreto Zavaleta, Jeanlee | 100% |
--- kd_tree.py
+++ kd_tree.py
@@ -1,11 +1,59 @@
 import numpy as np
 from scipy.spatial import KDTree
-x, y = np.mgrid[0:5, 2:8]
-df = np.c_[x.ravel(), y.ravel()]
-tree = KDTree(df)
-# v id
-# v 128
-print(tree.data)
-query = [0, 0]
-dd, ii = tree.query([query], k=3)
-print(dd, ii, sep='\n')
+import face_recognition
+import pandas as pd
+import pickle
+import time
+size = 90
+#Traning_Data
+dataset_path = "./data/dataset_PCA_90_90.csv"
+scaler_path = "./bin/scaler_PCA_90_90.dat"
+pca_path = "./bin/pca_PCA_90_90.dat"
+ncomponents_path = "./bin/ncomponents_PCA_90_90.dat"
+
+
+#Testing Data
+path_1 = "./DataSet/10/"
+image_path = path_1 + "S2_6.jpg"
+
+
+
+df = pd.read_csv(dataset_path)
+scaler = pickle.load(open(scaler_path, "rb"))
+pca = pickle.load(open(pca_path, "rb"))
+ncomponents = pickle.load(open(ncomponents_path, "rb"))
+
+#print("Completed")
+#df.tail(5)
+
+
+features = [str(i) for i in range(1, ncomponents+1)]
+x = df.loc[:, features]
+y = df.loc[:, ["path"]]
+#y.tail(5)
+
+tree = KDTree(x)
+
+def generate_df(x):
+    return pd.DataFrame(data=x, columns = [str(i) for i in range(1, 129)])
+
+def generate_point(v):
+    return tuple(np.concatenate([v, v], axis = None))
+
+def parser_image(image_path):
+    picture = face_recognition.load_image_file(image_path)    
+    all_face_encodings = face_recognition.face_encodings(picture)
+    x = generate_df(all_face_encodings)
+    x_scaled = scaler.transform(x)
+    x_pca = pca.transform(x_scaled)
+    return x_pca[0]
+
+
+image = parser_image(image_path)
+
+start = time.perf_counter()
+dd, ii = tree.query([image],k=1)
+end = time.perf_counter()
+#print(dd, ii, sep='\n')
+#print(df.iloc[ii[0]])
+print(f"Knearest with size: {size} elements finished in {end - start:0.7f} seconds")
